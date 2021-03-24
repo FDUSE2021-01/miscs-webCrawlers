db8e7fa9e4b606aef0b965e60fb706b28e3c2f27
xiaoas <xiaoqilin82@gmail.com>
2021-03-24 19:45:10 +0800

    init is there any deal grab lowest price history

---
+++ history.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import lib
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import argparse
+
+def main(config: dict):
+    itemName = lib.str2itadPlainName(config.itemName)
+    URI = f'https://isthereanydeal.com/ajax/game/info?plain=halflifealyx{pageNumber}'
+    text = lib.get_text(URI)
+    if not text:
+        print(URI, 'failed to load.')
+        return
+    frame = pd.read_html(text)
+    if not frame:
+        return
+    frame = frame[0]
+    lowestPrice = frame[frame['Store'] == 'Steam']['Lowest'].item()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='get lowest price of item on steam')
+    parser.add_argument('-i', '--item-name', dest='itemName',
+                        help='the name of the item',
+                        type=str)
+
+    args = parser.parse_args()
+    main(args)
+    
--- lib.py
+++ lib.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import re 
+
 def get_text(url):
     try:
         headers = {
@@ -13,3 +15,20 @@ def get_text(url):
         return r.text
     except:
         return ''
+
+def str2itadPlainName(appName: str) -> str:
+    appName = appName.lower()
+    appName = re.sub(r'[^a-z\d', '', appName)
+    covDict = {
+        '0': '0',
+        '1': 'i',
+        '2': 'ii',
+        '3': 'iii',
+        '4': 'iv',
+        '5': 'v',
+        '6': 'vi',
+        '7': 'vii',
+        '8': 'viii',
+        '9': 'ix',
+    }
+    return re.sub(r'\d', lambda x:covDict[x.group(0)], appName)
